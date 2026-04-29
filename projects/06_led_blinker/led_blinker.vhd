0c47b00bdff7e704d23c2c8fa7bca93ba222960d
mjvladika <140717810+mjvladika@users.noreply.github.com>
2026-04-28 21:24:04 -0400

    Cleaned up debugging code and size of count

--- projects/06_led_blinker/led_blinker.vhd
+++ projects/06_led_blinker/led_blinker.vhd
@@ -11,24 +11,21 @@ end led_blinker;
 
 architecture rtl of led_blinker is
 
-signal count : unsigned(26 downto 0) := "000000000000000000000000000";
+signal count : unsigned(5 downto 0) := "000000";
 signal led_toggle : std_logic := '0';
 
 begin
     process(clk, rst)
     begin
         if (rst = '1') then
-            count <= "000000000000000000000000000";
+            count <= "000000";
             led_toggle <= '0';
         elsif (rising_edge(clk)) then
             count <= count + 1;
 
-            report "COUNT: " & integer'image(to_integer(count));
-            report "LED_TOGGLE: " & std_logic'image(led_toggle);
-
             if (count = 50) then
                 led_toggle <= not led_toggle;
-                count <= "000000000000000000000000000";
+                count <= "000000";
             end if;
         end if;
     end process;
